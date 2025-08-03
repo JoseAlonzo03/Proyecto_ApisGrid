d2a480dba972ef794006ca2261b8600acb3acff6
José Ortega <josealonzoserrano@gmail.com>
2025-08-03 13:06:11 -0600

    Cambios finales

--- ProyectodeTeoríadelaComputación/PantallaInicio.Designer.cs
+++ ProyectodeTeoríadelaComputación/PantallaInicio.Designer.cs
@@ -36,6 +36,8 @@
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.label4 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
@@ -44,7 +46,7 @@
             this.btnNuevo.BackColor = System.Drawing.Color.SeaGreen;
             this.btnNuevo.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnNuevo.Image = global::ProyectodeTeoríadelaComputación.Properties.Resources.Nuevo;
-            this.btnNuevo.Location = new System.Drawing.Point(294, 370);
+            this.btnNuevo.Location = new System.Drawing.Point(294, 461);
             this.btnNuevo.Name = "btnNuevo";
             this.btnNuevo.Size = new System.Drawing.Size(208, 118);
             this.btnNuevo.TabIndex = 34;
@@ -91,7 +93,7 @@
             this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox2.BackgroundImage = global::ProyectodeTeoríadelaComputación.Properties.Resources.ApisGrid_Logo2;
             this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBox2.Location = new System.Drawing.Point(388, 210);
+            this.pictureBox2.Location = new System.Drawing.Point(388, 301);
             this.pictureBox2.Name = "pictureBox2";
             this.pictureBox2.Size = new System.Drawing.Size(263, 142);
             this.pictureBox2.TabIndex = 52;
@@ -102,7 +104,7 @@
             this.label4.AutoSize = true;
             this.label4.BackColor = System.Drawing.Color.Transparent;
             this.label4.Font = new System.Drawing.Font("Arial Rounded MT Bold", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(312, 153);
+            this.label4.Location = new System.Drawing.Point(312, 202);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(401, 32);
             this.label4.TabIndex = 53;
@@ -113,7 +115,7 @@
             this.button1.BackColor = System.Drawing.Color.SeaGreen;
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.Image = global::ProyectodeTeoríadelaComputación.Properties.Resources.Abrir;
-            this.button1.Location = new System.Drawing.Point(522, 370);
+            this.button1.Location = new System.Drawing.Point(522, 461);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(208, 118);
             this.button1.TabIndex = 55;
@@ -122,13 +124,37 @@
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Arial Rounded MT Bold", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(288, 247);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(451, 32);
+            this.label5.TabIndex = 56;
+            this.label5.Text = "Katerine Roxana Duarte Aguilar";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("Arial", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(442, 160);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(123, 33);
+            this.label6.TabIndex = 57;
+            this.label6.Text = "Grupo 6";
+            // 
             // PantallaInicio
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::ProyectodeTeoríadelaComputación.Properties.Resources.FondoInicio;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(1013, 507);
+            this.ClientSize = new System.Drawing.Size(1013, 591);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.pictureBox2);
@@ -157,5 +183,7 @@
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
     }
 }
